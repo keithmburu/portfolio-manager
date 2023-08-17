4fad6d117cea3fb4aa1dbf49951c0da72a4b91b5
WanqiLiang <wenkiliang@outlook.com>
2023-08-17 15:28:30 -0400

    updated table

--- createTables.sql
+++ createTables.sql
@@ -5,23 +5,25 @@ USE TAPHK;
 CREATE TABLE portfolio (
     id INT PRIMARY KEY auto_increment,
     asset_type VARCHAR(255) NOT NULL,
-    stock_ticker VARCHAR(20) NOT NULL,
-    company_name VARCHAR(255) NOT NULL,
+    asset_ticker VARCHAR(20),
+    asset_name VARCHAR(255) NOT NULL,
     volume INT NOT NULL,
-    start_datetime DATETIME NOT NULL
+    start_datetime DATETIME NOT NULL,
+    mature_datetime DATETIME,
+    currency VARCHAR(255),
 );
 
 
 CREATE TABLE asset_data (
     id INT PRIMARY KEY auto_increment,
     asset_type VARCHAR(255) NOT NULL,
-    stock_ticker VARCHAR(20) NOT NULL,
+    asset_ticker VARCHAR(20),
+    asset_name VARCHAR(255) NOT NULL,
     date DATETIME NOT NULL,
-    open_price FLOAT NOT NULL,
-    close_price FLOAT NOT NULL,
-    high_price FLOAT NOT NULL,
-    low_price FLOAT NOT NULL,
-    volume INT NOT NULL
+    open_price FLOAT,
+    close_price FLOAT,
+    high_price,
+    low_price
 );
 
 CREATE USER 'training'@'localhost' IDENTIFIED BY '1234567A';
