7510bb97a060ac3b46b4c313e35ec30a0f0f229f
WanqiLiang <wenkiliang@outlook.com>
2023-08-17 13:10:55 -0400

    first commit, created environment

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+import mysql.connector
+
+app = Flask(__name__)
+api = Api(app)
+
+
+db = mysql.connector.connect(
+        host="localhost",
+        user="nora_training",
+        password="1234567A",
+        database="conygre"
+    )
