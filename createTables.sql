df296e58e5606038d1aaa2e4d5fdbaf3068e3290
WanqiLiang <wenkiliang@outlook.com>
2023-08-17 14:43:09 -0400

    add table content: asset type and start date

--- createTables.sql
+++ createTables.sql
@@ -6,12 +6,15 @@ CREATE TABLE portfolio (
     id INT PRIMARY KEY auto_increment,
     asset_type VARCHAR(255) NOT NULL,
     stock_ticker VARCHAR(20) NOT NULL,
-    volume INT NOT NULL
+    company_name VARCHAR(255) NOT NULL,
+    volume INT NOT NULL,
+    start_datetime DATETIME NOT NULL,
 );
 
 
 CREATE TABLE asset_data (
     id INT PRIMARY KEY auto_increment,
+    asset_type VARCHAR(255) NOT NULL,
     stock_ticker VARCHAR(20) NOT NULL,
     date DATETIME NOT NULL,
     open_price FLOAT NOT NULL,
