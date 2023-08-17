6bd13413f50bf33be19afbc51f7b6294c455b649
Your Name <you@example.com>
2023-08-17 16:42:27 -0400

    changed type of asset_name to str

--- app.py
+++ app.py
@@ -15,16 +15,6 @@ db = mysql.connector.connect(
 
 
 class PortfolioResource(Resource):
-    # only for stock for now
-    def update_net_worth(self):
-        query = """
-        UPDATE portfolio P
-        JOIN (
-            SELECT P.asset_name,
-                    SUM
-        )
-        """
-        
     def get(self):
         cursor = db.cursor()
         cursor.execute('''SELECT * FROM portfolio''')
@@ -34,43 +24,14 @@ class PortfolioResource(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument("asset_type", required=True)
+        parser.add_argument("asset_type")
         parser.add_argument("asset_ticker")
-        parser.add_argument("asset_name", required=True)
-        parser.add_argument("amount_holding", required=True, type=int)
-        parser.add_argument("buy_datetime", required=True)
-        parser.add_argument("mature_datetime")
-        parser.add_argument("currency")
+        parser.add_argument("asset_name")
+        parser.add_argument("amount_holding")
         args = parser.parse_args()
-
-        asset_type = args["asset_type"].upper()
-        asset_ticker = args["asset_ticker"].upper()
-        asset_name = args["asset_name"].upper()
-        amount_holding = args["amount_holding"]
-        
-        try:
-            # Parse start_datetime using dateutil.parser
-            buy_datetime = dateutil.parser.parse(args["buy_datetime"])
-        except Exception as e:
-            return {"error": "Invalid buy_datetime format"}, 400
-        
-        if args["mature_datetime"]:
-            try:
-                # Parse mature_datetime using dateutil.parser
-                mature_datetime = dateutil.parser.parse(args["mature_datetime"])
-            except Exception as e:
-                return {"error": "Invalid mature_datetime format"}, 400
-        else:
-            mature_datetime = None
-
-        currency = args["currency"].upper() if args["currency"] else None
-        
         cursor = db.cursor()
-        cursor.execute('''INSERT INTO portfolio 
-                          (asset_type, asset_ticker, asset_name, amount_holding, start_datetime, mature_datetime, currency)
-                          VALUES (%s, %s, %s, %s, %s, %s, %s)''',
-                       (asset_type, asset_ticker, asset_name, amount_holding, buy_datetime, mature_datetime,currency))
-
+        cursor.execute('''INSERT INTO portfolio VALUES (%s, %s, %s, %s)''', \
+                        (args["asset_type"], args["stock_ticker"], args["company_name"], args["amount_holding"]))
         db.commit()
         cursor.close()
         return {"message": "Added new asset to portfolio"}, 201
@@ -127,7 +88,7 @@ api.add_resource(PortfolioResource, '/')
 api.add_resource(StocksResource, '/stocks')
 api.add_resource(BondsResource, '/bonds')
 api.add_resource(CashResource, '/cash')
-api.add_resource(AssetResource, '/asset/<int:asset_name>')
+api.add_resource(AssetResource, '/asset/<str:asset_name>')
 
 
 if __name__ == '__main__':
